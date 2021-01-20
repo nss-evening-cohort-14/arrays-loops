cae5e46ef4ae3478180817fdbfb96319cb1693eb
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-01-19 20:38:12 -0600

    did some array methods

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("CONNECTED");
+const array1 = ['cow', 'dog', 'cat'];
+const string1 = array1.join();
+const string1ToArray = string1.split(',');
+// console.log(string1);
+// console.log(string1ToArray);
+
+// .push() & .pop()
+// array1.pop();
+// console.log(array1);
+// array1.push('cat');
+// console.log(array1);
+
+// .unshift() & .shift()
+array1.unshift('cow');
+console.log(array1);
+array1.shift();
+console.log(array1);
