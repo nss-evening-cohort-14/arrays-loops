7c8831c3ecdbd0d1070c5fd4ded917a100d6c7bb
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-01-19 19:42:46 -0600

    working with arrays

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
 // console.log(students[1]); // Jackie
 // console.log(students[students.length - 1]); // Nathan
 
-students[0] = 'Matthew';
+// *********** ASSIGNMENT
+// students[0] = 'Matthew';
 // console.log(students);
 
+// *********** ARRAYS AND FUNCTIONS
+// valuePrinter accepts an array and a number
 const valuePrinter = (array, index) => {
   return array[index];
 }
 
 console.log(valuePrinter(students, 2));
+console.log(valuePrinter([1,2,3,4,5], 2));
 
 // Write a function that takes an array of something and tells you if the name 'Greg' is in that array
 
 
-// this function is accepting an array and a string
+// findStudent accepts an array and a string
 const findStudent = (array, student) => {
   return array.includes(student);
 }
